923e860917a2778c428c15378916d0547e80257b
YOONA <youna@coontec.com>
2025-06-28 15:49:27 +0900

    Add : cronjob

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -335,6 +335,9 @@ export const ROUTES = {
               { "title": "Deployment", "href": "/deployment" },
               { "title": "ReplicaSet", "href": "/replicaset" },
               { "title": "StatefulSet", "href": "/statefulset" },
+              { "title": "DaemonSet", "href": "/daemonset" },
+              { "title": "Job", "href": "/job" },
+              { "title": "CronJob", "href": "/cronjob" },
 
             ]
           },
